e398a4f21d31327858e68f82c77998043d39f125
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 02:16:21 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,17 +1,7 @@
 def square_array(array)
-  new_arr = []
-  array.each do |num|
-    if (Math.sqrt(num) % 1) == 0
-      new_arr << Math.sqrt(num).to_i
-    else
-      new_arr << (num * num)
-    end
-    return new_arr
+  newArr = []
+  array.each do |element|
+    newArr.push(Math.sqrt(element).to_i)
   end
-  puts new_arr
-  # newArr = []
-  # array.each do |element|
-  #   newArr.push(Math.sqrt(element).to_i)
-  # end
-  # return newArr
+  return newArr
 end
