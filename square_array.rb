709c17ca836c8d98cae1fa07dc1f99b27463d81e
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-09 02:37:10 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,4 @@ def square_array(array)
   arr = []
   array.each { |element| arr << element ** 2 }
   arr
-  # newArr = []
-  # array.each do |element|
-  #   newArr.push(Math.sqrt(element).to_i)
-  # end
-  # return newArr
 end
